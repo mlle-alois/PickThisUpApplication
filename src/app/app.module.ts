a2a61c42e689779157591b160759588d84e18697
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2021-05-19 18:07:22 +0200

    [CONNEXION]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClient} from "@angular/common/http";
+import {HttpModule} from '@angular/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {NgxLoadingModule} from 'ngx-loading';
+import {NgModule} from "@angular/core";
+import {ButtonModule} from "primeng/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpModule,
+    ReactiveFormsModule,
+    NgxLoadingModule.forRoot({}),
+    ButtonModule
+
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
